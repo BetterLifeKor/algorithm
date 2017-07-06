80878925981a5718a7c1c78cf34717aa7c0f443f
BetterLifeKor <betterlife.kor@gmail.com>
2017-07-06 23:25:11 +0900

    dfs update
    dfs update

---
+++ Graph/dfs.cpp
@@ -0,0 +1,24 @@
+vector < vector < int > > adj;
+
+vector<bool> visited;
+
+void dfs(int heree){
+	cout << "DFS visits " << here << endl;
+	visited[here] = true;
+
+	for (int i = 0; i < adf[here].size(); ++i){
+		int there = adj[here][i];
+
+		if(!visited[there])
+			dfs(there);
+	}
+}
+
+void dfsAll(){
+	visited = vector<bool> (adj.size(), false);
+
+	for(int i = 0; i < adj.size(); i++){
+		if(!visited[i])
+			dfs(i);
+	}
+}
